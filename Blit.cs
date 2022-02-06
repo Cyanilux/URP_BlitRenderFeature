08968f27c83f02522832eea8c1299d65145aa29e
Cyan <69320946+Cyanilux@users.noreply.github.com>
2022-02-06 22:20:40 +0000

    Setup package. Scripts also now under Cyan namespace

--- Blit.cs
+++ Blit.cs
@@ -19,155 +19,157 @@ using UnityEngine.Rendering.Universal;
  * ------------------------------------------------------------------------------------------------------------------------
  * @Cyanilux
 */
-[CreateAssetMenu(menuName = "Feature/Blit")]
-public class Blit : ScriptableRendererFeature {
 
-	public class BlitPass : ScriptableRenderPass {
+namespace Cyan {
+	[CreateAssetMenu(menuName = "Feature/Blit")]
+	public class Blit : ScriptableRendererFeature {
 
-		public Material blitMaterial = null;
-		public FilterMode filterMode { get; set; }
+		public class BlitPass : ScriptableRenderPass {
 
-		private BlitSettings settings;
+			public Material blitMaterial = null;
+			public FilterMode filterMode { get; set; }
 
-		private RenderTargetIdentifier source { get; set; }
-		private RenderTargetIdentifier destination { get; set; }
+			private BlitSettings settings;
 
-		RenderTargetHandle m_TemporaryColorTexture;
-		RenderTargetHandle m_DestinationTexture;
-		string m_ProfilerTag;
+			private RenderTargetIdentifier source { get; set; }
+			private RenderTargetIdentifier destination { get; set; }
 
-		public BlitPass(RenderPassEvent renderPassEvent, BlitSettings settings, string tag) {
-			this.renderPassEvent = renderPassEvent;
-			this.settings = settings;
-			blitMaterial = settings.blitMaterial;
-			m_ProfilerTag = tag;
-			m_TemporaryColorTexture.Init("_TemporaryColorTexture");
-			if (settings.dstType == Target.TextureID) {
-				m_DestinationTexture.Init(settings.dstTextureId);
-			}
-		}
+			RenderTargetHandle m_TemporaryColorTexture;
+			RenderTargetHandle m_DestinationTexture;
+			string m_ProfilerTag;
 
-		public void Setup() {
-#if UNITY_2020_1_OR_NEWER
-			if (settings.requireDepthNormals)
-				ConfigureInput(ScriptableRenderPassInput.Normal);
-#endif
-		}
-
-		public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
-			CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
-			RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
-			opaqueDesc.depthBufferBits = 0;
-
-			// Set Source / Destination
-			// note : Seems this has to be done in here rather than in AddRenderPasses to work correctly in 2021.2+
-			var renderer = renderingData.cameraData.renderer;
-
-			if (settings.srcType == Target.CameraColor){
-				source = renderer.cameraColorTarget;
-			}else if (settings.srcType == Target.TextureID){
-				source = new RenderTargetIdentifier(settings.srcTextureId);
-			}else if (settings.srcType == Target.RenderTextureObject){
-				source = new RenderTargetIdentifier(settings.srcTextureObject);
-			}
-
-			if (settings.dstType == Target.CameraColor){
-				destination = renderer.cameraColorTarget;
-			}else if (settings.dstType == Target.TextureID){
-				destination = new RenderTargetIdentifier(settings.dstTextureId);
-			}else if (settings.dstType == Target.RenderTextureObject){
-				destination = new RenderTargetIdentifier(settings.dstTextureObject);
-			}
-
-			if (settings.setInverseViewMatrix) {
-				Shader.SetGlobalMatrix("_InverseView", renderingData.cameraData.camera.cameraToWorldMatrix);
-			}
-
-			if (settings.dstType == Target.TextureID) {
-				if (settings.overrideGraphicsFormat){
-					opaqueDesc.graphicsFormat = settings.graphicsFormat;
+			public BlitPass(RenderPassEvent renderPassEvent, BlitSettings settings, string tag) {
+				this.renderPassEvent = renderPassEvent;
+				this.settings = settings;
+				blitMaterial = settings.blitMaterial;
+				m_ProfilerTag = tag;
+				m_TemporaryColorTexture.Init("_TemporaryColorTexture");
+				if (settings.dstType == Target.TextureID) {
+					m_DestinationTexture.Init(settings.dstTextureId);
 				}
-				cmd.GetTemporaryRT(m_DestinationTexture.id, opaqueDesc, filterMode);
 			}
 
-			//Debug.Log($"src = {source},     dst = {destination} ");
-			// Can't read and write to same color target, use a TemporaryRT
-			if (source == destination || (settings.srcType == settings.dstType && settings.srcType == Target.CameraColor)) {
-				cmd.GetTemporaryRT(m_TemporaryColorTexture.id, opaqueDesc, filterMode);
-				Blit(cmd, source, m_TemporaryColorTexture.Identifier(), blitMaterial, settings.blitMaterialPassIndex);
-				Blit(cmd, m_TemporaryColorTexture.Identifier(), destination);
-			} else {
-				Blit(cmd, source, destination, blitMaterial, settings.blitMaterialPassIndex);
+			public void Setup() {
+#if UNITY_2020_1_OR_NEWER
+				if (settings.requireDepthNormals)
+					ConfigureInput(ScriptableRenderPassInput.Normal);
+#endif
 			}
 
-			context.ExecuteCommandBuffer(cmd);
-			CommandBufferPool.Release(cmd);
+			public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
+				CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
+				RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+				opaqueDesc.depthBufferBits = 0;
+
+				// Set Source / Destination
+				// note : Seems this has to be done in here rather than in AddRenderPasses to work correctly in 2021.2+
+				var renderer = renderingData.cameraData.renderer;
+
+				if (settings.srcType == Target.CameraColor) {
+					source = renderer.cameraColorTarget;
+				} else if (settings.srcType == Target.TextureID) {
+					source = new RenderTargetIdentifier(settings.srcTextureId);
+				} else if (settings.srcType == Target.RenderTextureObject) {
+					source = new RenderTargetIdentifier(settings.srcTextureObject);
+				}
+
+				if (settings.dstType == Target.CameraColor) {
+					destination = renderer.cameraColorTarget;
+				} else if (settings.dstType == Target.TextureID) {
+					destination = new RenderTargetIdentifier(settings.dstTextureId);
+				} else if (settings.dstType == Target.RenderTextureObject) {
+					destination = new RenderTargetIdentifier(settings.dstTextureObject);
+				}
+
+				if (settings.setInverseViewMatrix) {
+					Shader.SetGlobalMatrix("_InverseView", renderingData.cameraData.camera.cameraToWorldMatrix);
+				}
+
+				if (settings.dstType == Target.TextureID) {
+					if (settings.overrideGraphicsFormat) {
+						opaqueDesc.graphicsFormat = settings.graphicsFormat;
+					}
+					cmd.GetTemporaryRT(m_DestinationTexture.id, opaqueDesc, filterMode);
+				}
+
+				//Debug.Log($"src = {source},     dst = {destination} ");
+				// Can't read and write to same color target, use a TemporaryRT
+				if (source == destination || (settings.srcType == settings.dstType && settings.srcType == Target.CameraColor)) {
+					cmd.GetTemporaryRT(m_TemporaryColorTexture.id, opaqueDesc, filterMode);
+					Blit(cmd, source, m_TemporaryColorTexture.Identifier(), blitMaterial, settings.blitMaterialPassIndex);
+					Blit(cmd, m_TemporaryColorTexture.Identifier(), destination);
+				} else {
+					Blit(cmd, source, destination, blitMaterial, settings.blitMaterialPassIndex);
+				}
+
+				context.ExecuteCommandBuffer(cmd);
+				CommandBufferPool.Release(cmd);
+			}
+
+			public override void FrameCleanup(CommandBuffer cmd) {
+				if (settings.dstType == Target.TextureID) {
+					cmd.ReleaseTemporaryRT(m_DestinationTexture.id);
+				}
+				if (source == destination || (settings.srcType == settings.dstType && settings.srcType == Target.CameraColor)) {
+					cmd.ReleaseTemporaryRT(m_TemporaryColorTexture.id);
+				}
+			}
 		}
 
-		public override void FrameCleanup(CommandBuffer cmd) {
-			if (settings.dstType == Target.TextureID) {
-				cmd.ReleaseTemporaryRT(m_DestinationTexture.id);
-			}
-			if (source == destination || (settings.srcType == settings.dstType && settings.srcType == Target.CameraColor)) {
-				cmd.ReleaseTemporaryRT(m_TemporaryColorTexture.id);
-			}
+		[System.Serializable]
+		public class BlitSettings {
+			public RenderPassEvent Event = RenderPassEvent.AfterRenderingOpaques;
+
+			public Material blitMaterial = null;
+			public int blitMaterialPassIndex = 0;
+			public bool setInverseViewMatrix = false;
+			public bool requireDepthNormals = false;
+
+			public Target srcType = Target.CameraColor;
+			public string srcTextureId = "_CameraColorTexture";
+			public RenderTexture srcTextureObject;
+
+			public Target dstType = Target.CameraColor;
+			public string dstTextureId = "_BlitPassTexture";
+			public RenderTexture dstTextureObject;
+
+			public bool overrideGraphicsFormat = false;
+			public UnityEngine.Experimental.Rendering.GraphicsFormat graphicsFormat;
 		}
-	}
 
-	[System.Serializable]
-	public class BlitSettings {
-		public RenderPassEvent Event = RenderPassEvent.AfterRenderingOpaques;
+		public enum Target {
+			CameraColor,
+			TextureID,
+			RenderTextureObject
+		}
 
-		public Material blitMaterial = null;
-		public int blitMaterialPassIndex = 0;
-		public bool setInverseViewMatrix = false;
-		public bool requireDepthNormals = false;
+		public BlitSettings settings = new BlitSettings();
+		public BlitPass blitPass;
 
-		public Target srcType = Target.CameraColor;
-		public string srcTextureId = "_CameraColorTexture";
-		public RenderTexture srcTextureObject;
+		public override void Create() {
+			var passIndex = settings.blitMaterial != null ? settings.blitMaterial.passCount - 1 : 1;
+			settings.blitMaterialPassIndex = Mathf.Clamp(settings.blitMaterialPassIndex, -1, passIndex);
+			blitPass = new BlitPass(settings.Event, settings, name);
 
-		public Target dstType = Target.CameraColor;
-		public string dstTextureId = "_BlitPassTexture";
-		public RenderTexture dstTextureObject;
-
-		public bool overrideGraphicsFormat = false;
-		public UnityEngine.Experimental.Rendering.GraphicsFormat graphicsFormat;
-	}
-
-	public enum Target {
-		CameraColor,
-		TextureID,
-		RenderTextureObject
-	}
-
-	public BlitSettings settings = new BlitSettings();
-	public BlitPass blitPass;
-
-	public override void Create() {
-		var passIndex = settings.blitMaterial != null ? settings.blitMaterial.passCount - 1 : 1;
-		settings.blitMaterialPassIndex = Mathf.Clamp(settings.blitMaterialPassIndex, -1, passIndex);
-		blitPass = new BlitPass(settings.Event, settings, name);
-
-		#if !UNITY_2021_2_OR_NEWER
+#if !UNITY_2021_2_OR_NEWER
 		if (settings.Event == RenderPassEvent.AfterRenderingPostProcessing) {
 			Debug.LogWarning("Note that the \"After Rendering Post Processing\"'s Color target doesn't seem to work? (or might work, but doesn't contain the post processing) :( -- Use \"After Rendering\" instead!");
 		}
-		#endif
+#endif
 
-		if (settings.graphicsFormat == UnityEngine.Experimental.Rendering.GraphicsFormat.None) {
-			settings.graphicsFormat = SystemInfo.GetGraphicsFormat(UnityEngine.Experimental.Rendering.DefaultFormat.LDR);
-		}
-	}
-
-	public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
-
-		if (settings.blitMaterial == null) {
-			Debug.LogWarningFormat("Missing Blit Material. {0} blit pass will not execute. Check for missing reference in the assigned renderer.", GetType().Name);
-			return;
+			if (settings.graphicsFormat == UnityEngine.Experimental.Rendering.GraphicsFormat.None) {
+				settings.graphicsFormat = SystemInfo.GetGraphicsFormat(UnityEngine.Experimental.Rendering.DefaultFormat.LDR);
+			}
 		}
 
-		#if !UNITY_2021_2_OR_NEWER
+		public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
+
+			if (settings.blitMaterial == null) {
+				Debug.LogWarningFormat("Missing Blit Material. {0} blit pass will not execute. Check for missing reference in the assigned renderer.", GetType().Name);
+				return;
+			}
+
+#if !UNITY_2021_2_OR_NEWER
 		// AfterRenderingPostProcessing event is fixed in 2021.2+ so this workaround is no longer required
 
 		if (settings.Event == RenderPassEvent.AfterRenderingPostProcessing) {
@@ -192,9 +194,10 @@ public class Blit : ScriptableRendererFeature {
 				settings.dstTextureId = "";
 			}
 		}
-		#endif
-		
-		blitPass.Setup();
-		renderer.EnqueuePass(blitPass);
+#endif
+
+			blitPass.Setup();
+			renderer.EnqueuePass(blitPass);
+		}
 	}
 }
--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 ## Blit Renderer Feature
-Tested with **2021.2.5f1 / URP v12.1.2**. Should still work with older versions, otherwise see branches for 2019-2021.1 version.
-<br /><br />
-Extended to allow options for :<br />
-• Specific access to selecting a **source** and **destination** (via current camera's color / texture id / render texture object)<br />
-• Setting a **_InverseView** matrix (cameraToWorldMatrix), for shaders that might need it to handle calculations from screen space to world. For example, reconstructing world position from depth, see : https://twitter.com/Cyanilux/status/1269353975058501636<br />
-• (2021.1/v10+) Enabling generation of DepthNormals (_CameraNormalsTexture)
-<br /><br />
-Based on the Blit (now renamed to FullscreenFeature/Pass) from the [UniversalRenderingExamples](https://github.com/Unity-Technologies/UniversalRenderingExamples/tree/master/Assets/Scripts/Runtime/RenderPasses)
+- Tested with **2021.2.5f1 / URP v12.1.2**. Should still work with older versions, otherwise see branches for 2019-2021.1 version.
+- Based on the Blit (now renamed to FullscreenFeature/Pass) from the [UniversalRenderingExamples](https://github.com/Unity-Technologies/UniversalRenderingExamples/tree/master/Assets/Scripts/Runtime/RenderPasses)
+
+### Setup:
+- Install via Package Manager → Add package via git URL : `https://github.com/Cyanilux/ShaderGraphVariables.git`
+- Alternatively, download and put the folder in your Assets
+
+### Features :
+- Applies fullscreen effects via shader/material
+- Specific access to selecting the **source** and **destination** targets (via Camera, TextureID or Render Texture object)<br />
+- Setting an **_InverseView** matrix (cameraToWorldMatrix), for shaders that might need it to handle calculations from screen space to world. For example, reconstructing world position from depth, see : https://twitter.com/Cyanilux/status/1269353975058501636<br />
+- (2021.1/v10+) Enabling generation of DepthNormals (_CameraNormalsTexture)
+
 <br /><br />
 @Cyanilux<br />
 :)
